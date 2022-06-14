5ab9f831d199139eeb00b6b8ef3f6f956b6fd126
DESKTOP-GQLE41P\Manna <hrkim@mannaplanet.co.kr>
2022-06-14 17:08:11 +0900

    update

--- src/views/page1.vue
+++ src/views/page1.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <div>
         <h1>page1</h1>
-        업데이트가 되었나?-2
+        업데이트가 되었나?-05:07
         <iframe width="100%" height="700" src="https://www.kakaocorp.com/page/kakao/history"></iframe>
 
        
